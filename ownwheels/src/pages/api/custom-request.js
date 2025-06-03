256a1cf576c6bb16c1f623fe84b4bc9aa42b8328
Husnain Haider Kazmi <165249748+hasnainkazmi512@users.noreply.github.com>
2025-06-04 00:25:04 +0500

    error

--- ownwheels/src/pages/api/custom-request.js
+++ ownwheels/src/pages/api/custom-request.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import styles from '../styles/CusRequests.module.css';
+import styles from '../styles/CusRequests.css';
 
 const CustomRequests = () => {
   const [requests, setRequests] = useState([]);
